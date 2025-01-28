b197cb2d529aa730d982eb3b01a47a099d419c6a
Beccaworsnop <120226362+Beccaworsnop@users.noreply.github.com>
2025-01-28 06:41:17 -0800

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -16,3 +16,37 @@ CREATE TABLE component_manager.sub_category (
     FOREIGN KEY (parent) REFERENCES component_manager.category(uuid)
 );
 
+CREATE TABLE component_manager.component(
+    uuid UUID PRIMARY KEY,
+    id VARCHAR(50) GENERATED ALWAYS AS (UPPER(SUBSTRING(category, 1, 2) || SUBSTRING(sub_category, 1, 2))) STORED,
+    reference VARCHAR(50) NOT NULL UNIQUE,
+    quantity INT CHECK (quantity >= 0) NOT NULL
+    date_checked DATE,
+    category VARCHAR(50) NOT NULL,
+    sub_category VARCHAR(50) NOT NULL,
+    --contraint 
+    FOREIGN KEY (category)
+    REFERENCES component_manager.category(categoryName),
+    --constraint AGAIN 
+     FOREIGN KEY(sub_category)
+     REFERENCES component_manager.sub_category(categoryName)
+);
+
+CREATE TABLE members(
+    uuid UUID PRIMARY KEY,             
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE component_manager.sub_component(
+     uuid UUID PRIMARY KEY,
+     super_uuid UUID NOT NULL,
+     place UUID NOT NULL,
+     note VARCHAR(500) ,
+     --constraint
+     FOREIGN KEY(super_uuid)
+     REFERENCES component_manager.component(uuid),
+     --CONSTRAINT place_const
+     FOREIGN KEY (place)
+     REFERENCES members(uuid)
+);
