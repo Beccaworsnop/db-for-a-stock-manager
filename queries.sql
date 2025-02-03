88f51e7fb218c62312f5f4034e56dbadfb4db7ca
Beccaworsnop <120226362+Beccaworsnop@users.noreply.github.com>
2025-02-03 05:38:05 -0800

    Update queries.sql

---
+++ queries.sql
@@ -0,0 +1,20 @@
+INSERT INTO members(uuid, first_name) VALUES(uuid('00000000-0000-0000-0000-000000000000'), 'companyname');
+
+CREATE OR REPLACE FUNCTION component_manager.issouCrush(ref CITEXT)
+RETURNS CITEXT
+LANGUAGE plpgsql
+as
+$$ 
+declare
+    indexCount INT;
+BEGIN
+
+    SELECT position(' ' IN ref)
+    INTO indexCount;
+    IF indexCount = 0 THEN
+        RETURN UPPER(SUBSTRING(ref, 1, 2));
+    ELSE
+        RETURN UPPER((SUBSTRING(ref, 1, 1) || SUBSTRING(ref, indexCount+1, 1)));
+    END IF;
+END;
+$$;
