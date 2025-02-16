b77ebf7e23fbcfde2c5ffbf2fbb9b136eb5a18bd
Beccaworsnop <120226362+Beccaworsnop@users.noreply.github.com>
2025-02-16 04:59:25 -0800

    Update tables.sql
    citext instead of varchar

--- tables.sql
+++ tables.sql
@@ -2,26 +2,25 @@ CREATE DATABASE stock_manager;
 
 CREATE SCHEMA component_manager;
 
-
--- Create 5 tables with UUID as primary key
+CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE component_manager.category (
     uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    categoryName VARCHAR(50) NOT NULL UNIQUE -- ADD CITEXT INSTEAD OF VARCHAR ? 
+    category_name CITEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE component_manager.sub_category (
     uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    subCategoryName VARCHAR(50) NOT NULL, 
+    sub_category_name CITEXT NOT NULL, 
     parent UUID NOT NULL,
     FOREIGN KEY (parent) REFERENCES component_manager.category(uuid)
 );
 
 CREATE TABLE component_manager.component (
-    uuid UUID DEFAULT gen_random_uuid PRIMARY KEY,
-    ID VARCHAR(50) GENERATED ALWAYS AS 
+    uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    ID TEXT GENERATED ALWAYS AS 
        (UPPER(SUBSTRING(category::text, 1, 2) || SUBSTRING(sub_category::text, 1, 2))) STORED,
-    reference VARCHAR(50) NOT NULL UNIQUE,
+    reference CITEXT NOT NULL UNIQUE,
     quantity INT CHECK (quantity >= 0) NOT NULL,  
     date_checked DATE,  
     category UUID NOT NULL,
@@ -36,20 +35,20 @@ CREATE TABLE component_manager.component (
 
 CREATE TABLE component_manager.members (
     uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name  VARCHAR(50) NOT NULL
+    first_name CITEXT NOT NULL,
+    last_name  CITEXT NOT NULL
 );
 
 CREATE TABLE component_manager.sub_component (
-     uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    --place UUID NOT NULL DEFAULT uuid('00000000-0000-0000-0000-000000000000'),
-     super_uuid UUID NOT NULL,
-     place UUID NOT NULL,
-     note VARCHAR(500),
-     CONSTRAINT super_uuid_const
-     FOREIGN KEY (super_uuid)
-     REFERENCES component_manager.component(uuid),
-     CONSTRAINT place_const
-     FOREIGN KEY (place)
-     REFERENCES component_manager.members(uuid)
+    uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    place UUID NOT NULL DEFAULT uuid('00000000-0000-0000-0000-000000000000'),
+    super_uuid UUID NOT NULL,
+    place UUID NOT NULL,
+    note CITEXT,  
+    CONSTRAINT super_uuid_const
+        FOREIGN KEY (super_uuid)
+        REFERENCES component_manager.component(uuid),
+    CONSTRAINT place_const
+        FOREIGN KEY (place)
+        REFERENCES component_manager.members(uuid)
 );
