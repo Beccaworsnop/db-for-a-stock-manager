7ad89d55a4e33cd647cca26f567607374300e040
Beccaworsnop <120226362+Beccaworsnop@users.noreply.github.com>
2025-03-08 13:44:42 -0500

    Update roles.sql

--- roles.sql
+++ roles.sql
@@ -1,21 +1,23 @@
 CREATE ROLE user WITH LOGIN PASSWORD 'password!';
 CREATE ROLE student WITH LOGIN PASSWORD 'password';
+CREATE ROLE stock_manager;
 
 REVOKE ALL ON DATABASE stock_manager FROM student;
 GRANT CONNECT ON DATABASE stock_manager TO student;
 
 GRANT USAGE ON SCHEMA component_manager TO user;
 GRANT USAGE ON SCHEMA component_manager TO student;
+GRANT USAGE ON SCHEMA component_manager TO stock_manager;
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA component_manager TO student;
 
-GRANT SELECT ON safe_to_see_component TO user;
-
-CREATE ROLE stock_manager;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA component_manager TO stock_manager;
-GRANT USAGE ON SCHEMA component_manager TO stock_manager;
 
-ALTER TABLE safe_to_see_component ENABLE ROW LEVEL SECURITY;
-CREATE POLICY student_policy ON safe_to_see_component
+GRANT SELECT ON ALL TABLES IN SCHEMA component_manager TO user;
+
+ALTER TABLE component_manager.component ENABLE ROW LEVEL SECURITY;
+
+
+CREATE POLICY student_policy ON component_manager.component
 FOR SELECT TO student
-USING (true); 
+USING (true);
