8caa281ee3607123f467c628927543d24ca0823d
Beccaworsnop <120226362+Beccaworsnop@users.noreply.github.com>
2025-03-08 13:41:19 -0500

    Update tables.sql
    deleted members

--- tables.sql
+++ tables.sql
@@ -2,7 +2,7 @@ CREATE DATABASE stock_manager;
 
 CREATE SCHEMA component_manager;
 
-CREATE EXTENSION IF NOT EXISTS citext;
+CREATE EXTENSION IF NOT EXISTS CITEXT;
 
 CREATE TABLE component_manager.category (
     uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
@@ -11,7 +11,7 @@ CREATE TABLE component_manager.category (
 
 CREATE TABLE component_manager.sub_category (
     uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    sub_category_name CITEXT NOT NULL, 
+    sub_category_name CITEXT NOT NULL,
     parent UUID NOT NULL,
     FOREIGN KEY (parent) REFERENCES component_manager.category(uuid)
 );
@@ -21,8 +21,8 @@ CREATE TABLE component_manager.component (
     ID TEXT GENERATED ALWAYS AS 
        (UPPER(SUBSTRING(category::text, 1, 2) || SUBSTRING(sub_category::text, 1, 2))) STORED,
     reference CITEXT NOT NULL UNIQUE,
-    quantity INT CHECK (quantity >= 0) NOT NULL,  
-    date_checked DATE,  
+    quantity INT CHECK (quantity >= 0) NOT NULL,
+    date_checked DATE,
     category UUID NOT NULL,
     sub_category UUID NOT NULL,
     CONSTRAINT category_const
@@ -33,22 +33,14 @@ CREATE TABLE component_manager.component (
         REFERENCES component_manager.sub_category(uuid)
 );
 
-CREATE TABLE component_manager.members (
-    uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    first_name CITEXT NOT NULL,
-    last_name  CITEXT NOT NULL
-);
-
 CREATE TABLE component_manager.sub_component (
     uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    place UUID NOT NULL DEFAULT uuid('00000000-0000-0000-0000-000000000000'),
     super_uuid UUID NOT NULL,
-    place UUID NOT NULL,
-    note CITEXT,  
+    place CITEXT NOT NULL, 
+    note CITEXT,
     CONSTRAINT super_uuid_const
         FOREIGN KEY (super_uuid)
-        REFERENCES component_manager.component(uuid),
-    CONSTRAINT place_const
-        FOREIGN KEY (place)
-        REFERENCES component_manager.members(uuid)
+        REFERENCES component_manager.component(uuid)
 );
+
+
